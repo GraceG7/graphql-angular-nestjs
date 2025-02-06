f170adad5c43e70446778c1bd1e530b38a78ff5b
Grace Gan <grace@thred.co.nz>
2025-02-07 10:39:43 +1300

    apply compression

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import compression = require('compression');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(compression());
   await app.listen(3000);
 }
+
 bootstrap();
